4ccf73e74171089c5e59667938171a055d2f21f1
lolchicer <lolchicer@live.ru>
2023-12-14 18:30:24 +0300

    IAngleRange в Anglerange.cs

--- Geometry/AngleTuple.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.Xna.Framework;
-using System;
-
-using AngleTuple = (float Lesser, float Greater);
-
-namespace monogametest;
--- Geometry/Anglerange.cs
+++ Geometry/Anglerange.cs
@@ -1,46 +1,109 @@
 using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 
 namespace monogametest;
 
 public static class янехочувставлятьэтовContainsExtensionsпокачто
 {
-    public static bool Contains(this (float Angle, Anglerange.Sign Sign) containing, float angle) =>
+    public static bool Contains(this (float Angle, TupleAngleRange.Sign Sign) containing, float angle) =>
     containing.Sign switch
     {
         // не хватает отдельного типа для углов
-        Anglerange.Sign.Lesser => angle.GetAngle() <= containing.Angle,
-        Anglerange.Sign.Greater => angle.GetAngle() >= containing.Angle,
+        TupleAngleRange.Sign.Lesser => angle.GetAngle() <= containing.Angle,
+        TupleAngleRange.Sign.Greater => angle.GetAngle() >= containing.Angle,
         _ => false
     };
 
-    public static bool Contains(this (float Lesser, float Greater) containing, float angle) =>
-    containing.Lesser <= angle.GetAngle() &&
-    containing.Greater >= angle.GetAngle();
 
-    public static (float Lesser, float Greater) Aboba(float lesser, float greater)
+
+
+
+    public static bool Contains(this IEnumerable<(float Lesser, float Greater)> containing, float angle) =>
+    containing.Any(tuple => tuple.Contains(angle));
+}
+
+public interface IAngleRange
+{
+    public enum Sign
     {
-        if (greater - lesser >= 360)
-            return (0, 360);
-        else
-            return (lesser.GetAngle(), greater.GetAngle());
+        Lesser,
+        Greater
+    }
+
+    public struct Constraint
+    {
+        public Angle Angle;
+        public Sign Sign;
+    }
+
+    public bool Contains(Angle value);
+    public IEnumerable<Constraint> Constraints { get; }
+}
+
+public class CompleteAngleRange : IAngleRange
+{
+    public bool Contains(Angle value) => true;
+
+    public IEnumerable<IAngleRange.Constraint> Constraints => new IAngleRange.Constraint[] {
+        new() { Angle = new() { Value = 0 }, Sign = IAngleRange.Sign.Greater },
+        new() { Angle = new() { Value = 360 }, Sign = IAngleRange.Sign.Lesser } };
+}
+
+public class TupleAngleRange : IAngleRange
+{
+    private struct AngleTuple
+    {
+        public Angle Lesser = new();
+        public Angle Greater = new();
+
+        public bool Contains(Angle angle) =>
+        Lesser.Value <= angle.Value &&
+        Greater.Value >= angle.Value;
+
+        public AngleTuple() { }
+    }
+
+    private List<(float, Sign)> _constraints = new();
+
+    private List<AngleTuple> _tuples = new();
+
+    public TupleAngleRange() { }
+    private TupleAngleRange(IEnumerable<AngleTuple> tuples)
+    {
+        _tuples.AddRange(tuples);
+    }
+
+    public bool Contains(Angle angle)
+    {
+        var constraints = new List<(float, Sign)>();
+
+        constraints.AddRange(new[] { (0, Sign.Greater), (0, Sign.Lesser) })
+    }
+
+    public static bool Complete(this AngleTuple value1, AngleTuple value2)
+    {
+        
     }
 
     // стоит избавиться от этого
-    public static IEnumerable<(float Lesser, float Greater)> Sum(this (float Lesser, float Greater) value1, (float Lesser, float Greater) value2)
+    private static IAngleRange Sum(this AngleTuple value1, AngleTuple value2) => (
+    value1.Lesser.Value <= value2.Greater.Value,
+    value2.Lesser.Value <= value2.Greater.Value) switch
     {
-        if (value1.Lesser <= value2.Greater || value2.Lesser <= value2.Greater)
-            return new[] { (Math.Min(value1.Lesser, value2.Lesser), Math.Max(value1.Greater, value2.Greater)) };
-        else
-            return new[] { value1, value2 };
-    }
+        (true, true) => new CompleteAngleRange(),
+        (false, false) => new TupleAngleRange(new[] { value1, value2 }),
+        _ => new TupleAngleRange(new[] { new AngleTuple() {
+            Lesser = new Angle() { Value = Math.Min(value1.Lesser.Value, value2.Lesser.Value) },
+            Greater = new Angle() { Value = Math.Max(value1.Greater.Value, value2.Greater.Value) } } })
+    };
 
     // вэ тфэ
-    public static IEnumerable<(float Lesser, float Greater)> Sum(this IEnumerable<(float Lesser, float Greater)> value)
+    private static IAngleRange Sum(IAngleRange value)
     {
-        foreach (var value1 in value)
+        foreach (var value1 in value.Constraints)
         {
             foreach (var value2 in value.Except(new[] { value1 }))
             {
@@ -56,28 +119,5 @@ public static class янехочувставлятьэтовContainsExtensionsп
         }
 
         return value;
-    }
-
-    public static bool Contains(this IEnumerable<(float Lesser, float Greater)> containing, float angle) =>
-    containing.Any(tuple => tuple.Contains(angle));
-}
-
-public class Anglerange
-{
-    public enum Sign
-    {
-        Lesser,
-        Greater
-    }
-
-    private List<(float, Sign)> _constraints = new();
-
-
-
-    private bool Contains(float angle)
-    {
-        var constraints = new List<(float, Sign)>();
-
-        constraints.AddRange(new[] { (0, Sign.Greater), (0, Sign.Lesser) })
     }
 }
